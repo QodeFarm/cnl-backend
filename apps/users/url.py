05069909dc1c79ce89a1b2fd4ce0145402577437
Chetan0178 <chetan.chaudhari@qodefarm.com>
2025-02-06 12:15:22 +0530

    added flag admin_update so we can create user, update current user data and also we can update other user data if current user is only admin

--- apps/users/url.py
+++ apps/users/url.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import get_user_model
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views as jwtview
-from apps.users.views import UserManageView, RolePermissionsCreateView, CustomUserActivationViewSet, CustomUserCreateViewSet, RoleViewSet, ModulesViewSet, ActionsViewSet, ModuleSectionsViewSet, RolePermissionsViewSet, SendPasswordResetEmailView, UserChangePasswordView, UserPasswordResetView, UserTimeRestrictionsViewSet, UserAllowedWeekdaysViewSet, UserLoginView, UserRoleViewSet, UserAccessAPIView, UserUpdateByAdminOnlyAPIView
+from apps.users.views import UserManageView, RolePermissionsCreateView, CustomUserActivationViewSet, CustomUserCreateViewSet, RoleViewSet, ModulesViewSet, ActionsViewSet, ModuleSectionsViewSet, RolePermissionsViewSet, SendPasswordResetEmailView, UserChangePasswordView, UserPasswordResetView, UserTimeRestrictionsViewSet, UserAllowedWeekdaysViewSet, UserLoginView, UserRoleViewSet, UserAccessAPIView
 
 router = DefaultRouter()
 
@@ -29,10 +29,10 @@ urlpatterns = [
     path('activation/<uid>/<token>/', CustomUserActivationViewSet.as_view({'post': 'activation'}), name='activation'),
     path('role_permissions/', RolePermissionsCreateView.as_view(), name='load-role-permissions'),
     path('role_permissions/<uuid:role_id>/', RolePermissionsCreateView.as_view(), name='load-role-permissions'),
-    path('user/<uuid:user_id>/', UserManageView.as_view(), name='get/update-perticuler-user'),
+    path('user/<uuid:user_id>/', UserManageView.as_view(), name='get-perticuler-user'),
     path('user/', UserManageView.as_view(), name='get-all-users'),
     path('user_access/<str:role_id>/', UserAccessAPIView.as_view(), name='user-access'),
-    path('users_update/<uuid:user_id>/', UserUpdateByAdminOnlyAPIView.as_view(), name='update-user-by-admin'),
+    # path('users_update/<uuid:user_id>/', UserUpdateByAdminOnlyAPIView.as_view(), name='update-user-by-admin'),
 ]
 urlpatterns  += router.urls
 
