9ab73af4b312b3a04a9462ffbb83c53883f977c9
Asadupramod012 <pramodkumar.asadu@qodefarm.com>
2025-04-04 11:32:50 +0530

    updated settings.py Db Password

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "devcnl",
         "USER": "root",
-        "PASSWORD":"",
+        "PASSWORD":"root",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
