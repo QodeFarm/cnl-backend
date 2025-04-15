09b83a6857460a2d533a4e1c239ab2a16e742899
Chetan0178 <chetan.chaudhari@qodefarm.com>
2025-04-15 16:12:06 +0530

    added GET mothod that will return customer balance

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -1,17 +1,18 @@
-#add your urls 
-# from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
 from .views import *
+from rest_framework import routers
+from django.urls import path, include
+
 
 router = routers.DefaultRouter()
-router.register(r'ledger_accounts', LedgerAccountsViews)
 router.register(r'customer', CustomerViews)
+router.register(r'ledger_accounts', LedgerAccountsViews)
 router.register(r'customers_addresses', CustomerAddressesViews)
 router.register(r'customers_attachments', CustomerAttachmentsViews)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('customers/', CustomerCreateViews.as_view(), name='customers-create'),
-    path('customers/<str:pk>/', CustomerCreateViews.as_view(), name='customers-details')
+    path('customers/<str:pk>/', CustomerCreateViews.as_view(), name='customers-details'),
+    path('customers_balance/', CustomerBalanceView.as_view(), name='get-all-customers-balance'),
+    path('customers_balance/<str:pk>/', CustomerBalanceView.as_view(), name='get-customer-balance'),
 ]
