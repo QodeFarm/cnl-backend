1a0ddb3208586c48ba89b7b384d864dcf454f491
Chetan0178 <chetan.chaudhari@qodefarm.com>
2025-05-12 10:53:33 +0530

    added signals for loading data in JournalEntryLines after new records is created in SaleInvoiceOrders

--- apps/sales/signals.py
+++ apps/sales/signals.py
@@ -1,8 +1,10 @@
+from apps.finance.models import JournalEntryLines, ChartOfAccounts
 from .models import SaleInvoiceOrders, PaymentTransactions
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import transaction
+from django.db.models import Sum
 import logging
 
 # Set up basic configuration for logging
@@ -17,9 +19,29 @@ def update_balance_after_invoice(sender, instance, created, **kwargs):
     """
     Signal to update pending_amount with total_amount when a new SaleInvoiceOrders record is created.
     """
-    if created:  # Ensure it's a new record
+    if created:  # Ensuring it's a new record
         instance.pending_amount = instance.total_amount  # Copy total_amount to pending_amount
         instance.save(update_fields=['pending_amount'])  # Save only the pending_amount field
+
+        # Step 2: Calculating total pending (balance_amount) for that customer
+        total_pending = SaleInvoiceOrders.objects.filter(customer_id=instance.customer_id).aggregate(
+            total_pending=Sum('pending_amount'))['total_pending'] or 0.00
+
+        # Step 3: Get "sale account" from ChartOfAccounts
+        try:
+            sale_account = ChartOfAccounts.objects.get(account_name__iexact="Sale Account", is_active=True)
+        except ChartOfAccounts.DoesNotExist:
+            sale_account = None  
+
+        # Step 3: Creating JournalEntryLines record
+        JournalEntryLines.objects.create(
+            account_id=sale_account,  
+            debit=instance.total_amount,
+            credit=0.00,
+            description=f"Goods sold to {instance.customer_id.name}",
+            customer_id=instance.customer_id,
+            balance=total_pending
+        )
 
 
 @receiver(post_save, sender=PaymentTransactions)
