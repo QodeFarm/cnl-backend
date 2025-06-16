e9082ced84ba8d315948500712092c1c409f2309
Asadupramod012 <pramodkumar.asadu@qodefarm.com>
2025-06-16 13:02:17 +0530

    added in fields

--- apps/sales/filters.py
+++ apps/sales/filters.py
@@ -89,7 +89,7 @@ class SaleOrderFilter(filters.FilterSet):
     class Meta:
         model = SaleOrder 
         #do not change "order_no",it should remain as the 0th index. When using ?summary=true&page=1&limit=10, it will retrieve the results in descending order.
-        fields = ['order_no','order_date','sale_estimate','flow_status_name','customer_id','customer','sale_type_id','sale_type','order_status_id', 'status_name','created_at','advance_amount','tax','amount','period_name','s','sort','page','limit']
+        fields = ['order_no','order_date','sale_estimate','flow_status_name','customer_id','customer','sale_type_id','sale_type','order_status_id', 'status_name','created_at','advance_amount','tax','amount','period_name','work_order_created','s','sort','page','limit']
 
 class SaleInvoiceOrdersFilter(filters.FilterSet):
     customer_id = filters.CharFilter(method=filter_uuid)
