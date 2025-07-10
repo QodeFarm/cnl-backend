dc08f1f86be98379a0f2d845c368cc9392b2f952
Asadupramod012 <pramodkumar.asadu@qodefarm.com>
2025-07-10 13:43:06 +0530

    fixed all isuses in purchase modules.

--- apps/masters/template/purchase/purchase_doc.py
+++ apps/masters/template/purchase/purchase_doc.py
@@ -6,7 +6,7 @@ from apps.masters.utils.docs_variables import doc_data
 from apps.company.serializers import CompaniesSerializer
 from config.utils_methods import extract_product_data, format_phone_number,get_related_data
 
-def purchase_data(pk, document_type):
+def purchase_data(pk, document_type, format_value=None):
       #Companies Details
         company = Companies.objects.all()
         serializer = CompaniesSerializer(company, many=True)
@@ -41,7 +41,15 @@ def purchase_data(pk, document_type):
         shipping_address =  serializer(obj).data.get('shipping_address')
         order_no =  serializer(obj).data.get(number_value)
         order_date =  serializer(obj).data.get(date_value)
-        tax_type = serializer(obj).data.get('tax')
+        # tax_type = serializer(obj).data.get('tax')
+        
+        # Override tax_type display based on format selection
+        if format_value == 'CNL_Standard_Incl':
+            print("We are in the method...1")
+            tax_type = 'Inclusive'
+        elif format_value == 'CNL_Standard_Excl':
+            print("We are in the method...2")
+            tax_type = 'Exclusive'
 
         
         # Retrieve related data
@@ -49,19 +57,59 @@ def purchase_data(pk, document_type):
         shipments_data = get_related_data(related_model, related_serializer, related_filter_field , pk)
         shipments_data = shipments_data[0] if len(shipments_data)>0 else {}
 
-        total_amt = total_qty = total_txbl_amt = total_disc_amt = total_sub_amt = total_bill_amt = net_value = 0.0
+        # total_amt = total_qty = total_txbl_amt = total_disc_amt = total_sub_amt = total_bill_amt = net_value = 0.0
+        total_amt = total_qty = total_txbl_amt = cgst = sgst = igst = total_cgst = total_sgst= total_igst = cessAmt = total_disc_amt = round_0ff = party_old_balance = net_value = 0.0
 
         for item in items_data:
             total_amt += float(item['amount']) if item['amount'] is not None else 0
             total_qty += float(item['quantity']) if item['quantity'] is not None else 0
-            total_disc_amt += float(item['discount']) if item['discount'] is not None else 0
-            total_txbl_amt += float(item['tax']) if item['tax'] is not None else 0
+        #     total_disc_amt += float(item['quantity']) * float(item['rate']) * float(item['discount']) / 100
+        #     total_txbl_amt += float(item['tax']) if item['tax'] is not None else 0
+        
+            cgst = float(item['cgst']) if item['cgst'] is not None else 0
+            sgst = float(item['sgst']) if item['sgst'] is not None else 0
+            igst = float(item['igst']) if item['igst'] is not None else 0
 
-        total_sub_amt = total_amt - total_disc_amt  #Subtotal is the total amount before adding tax and after applying the discount.(Amount - Discount)
+            # Handle intra-state or inter-state
+            if billing_address and 'Andhra Pradesh' in billing_address:
+                print("-"*20)
+                print("we are in the method ...")
+                total_cgst += cgst  # Intra-state, split equally between CGST & SGST
+                print("total_cgst : ", total_cgst)
+                total_sgst += sgst
+                print("total_sgst : ", total_sgst)
+            else:
+                total_igst += igst  # Inter-state, tax goes to IGST
+                print("total_igst : ", total_igst)
+                print("-"*20)
 
-        total_bill_amt = total_sub_amt + total_txbl_amt #Bill Total is the final amount after adding the fixed tax.
+            total_disc_amt += float(item['quantity']) * float(item['rate']) * float(item['discount']) / 100
 
-        product_data = extract_product_data(items_data)
+
+
+        
+        # product_data = extract_product_data(items_data)
+        product_data = extract_product_data(items_data, tax_type=tax_type)
+        
+        discountAmt = serializer(obj).data.get('dis_amt')
+        discountAmt = float(discountAmt) if discountAmt is not None else 0.0  # Convert to float
+        # final_tax = total_cgst + total_sgst + total_igst
+        finalDiscount = 0
+        finalDiscount = discountAmt + total_disc_amt
+        itemstotal=0 #making itemstotal value 0.            
+            # itemstotal = customer_data_for_cust_data.get('item_value')
+        itemstotal += float(serializer(obj).data.get('item_value')) if serializer(obj).data.get('item_value') is not None else 0
+        cessAmt = serializer(obj).data.get('cess_amount')
+        cessAmt = round(float(cessAmt) if cessAmt is not None else 0.0, 2)  # Convert to float
+        
+        final_total = round(itemstotal - total_disc_amt, 2)
+
+        
+        final_amount = round(itemstotal + total_cgst + total_sgst + total_igst + cessAmt - total_disc_amt, 2)
+        total_sub_amt = itemstotal
+        total_bill_amt = final_amount
+
+        # product_data = extract_product_data(items_data)
 
         return {
                 'cust_bill_dtl' : 'Vendor Name & Address',
@@ -92,8 +140,17 @@ def purchase_data(pk, document_type):
 
                 'product_data' : product_data,
 
+                # 'total_amt' : total_amt,
+                # 'total_qty' : total_qty,
+                'itemstotal' : itemstotal,
+                'final_total': final_total,
                 'total_amt' : total_amt,
                 'total_qty' : total_qty,
+                'total_cgst' : round(total_cgst, 2),
+                'total_sgst' : round(total_sgst, 2),
+                'total_igst' : round(total_igst, 2),
+                
+                'finalDiscount' : finalDiscount,
                 'total_txbl_amt' : total_txbl_amt,
                 'total_disc_amt' : total_disc_amt,
                 'total_sub_amt'  : total_sub_amt,
@@ -119,11 +176,12 @@ def purchase_doc(
          elements, doc, company_name, company_address, company_phone, cust_bill_dtl, number_lbl, number_value, date_lbl, date_value,
          customer_name, v_billing_address, v_shipping_address_lbl, v_shipping_address,
          product_data,
-         total_qty, total_amt, total_disc, total_txbl_amt, total_sub_amt, total_bill_amt,
+        #  pdf_data['total_qty'], pdf_data['itemstotal'],  pdf_data['total_disc_amt'], pdf_data['final_total'], pdf_data['total_cgst'], pdf_data['total_sgst'], pdf_data['total_igst'],  pdf_data['total_amt'],
+         total_qty, itemstotal, total_disc_amt, total_cgst, total_sgst, total_igst, total_amt,  final_total, total_txbl_amt, total_sub_amt, total_bill_amt,
          destination,tax_type, shipping_mode_name, port_of_landing, port_of_discharge,
          comp_name,
          shipping_company_name, shipping_tracking_no , vehicle_vessel, no_of_packets, shipping_date, shipping_charges, weight,
-         comp_address, comp_phone, comp_email,
+         comp_address, comp_phone, comp_email
         ):
     
     # 1. Add company header
@@ -146,11 +204,12 @@ def purchase_doc(
     ))
 
     # Append product details
-    elements.append(product_details(product_data))
+    elements.append(product_details(product_data, show_gst=(tax_type != 'Inclusive')))
 
     # Append product total details
-    elements.append(product_total_details(
-        total_amt, total_disc, total_txbl_amt
+    elements.append(product_total_details_purchase(
+        # total_qty, total_amt, total_disc_amt, total_amt, show_gst=(tax_type != 'Inclusive')
+        total_qty, itemstotal, total_disc_amt, final_total, show_gst=(tax_type != 'Inclusive')
     ))
 
     elements.append(narration_and_total(
