cbb5240abfcc86be0481c09b79ae7333e8fdc576
Chetan0178 <chetan.chaudhari@qodefarm.com>
2025-07-14 16:43:12 +0530

    Testing part1

--- apps/users/user_test.py
+++ apps/users/user_test.py
@@ -1,16 +1,21 @@
+import pytest
+
+
 def test_user():
     mass = "hi"
     assert mass == "hi", "Failed"
 
 def test_user2():
-    mass = "Hello"
+    mass = "hello"
     assert mass == "hi", "Failed"
 
+@pytest.mark.xfail(reason="Feature not implemented yet")
 def test_user3():
+    print("HIIIIIIIIIIIIIIIIii")
     mass = "hi"
     assert mass == "hi", "Failed"
 
 def test_user4():
-    mass = "Hello"
-    print(mass)
-    assert mass == "hi", "Failed"
+    a =9
+    b = 10
+    assert a+10 == 19 , "Failed"
