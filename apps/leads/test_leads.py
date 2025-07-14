27cc37da998cc1c2c9f7e6b88aab932f6abe1ec7
Chetan0178 <chetan.chaudhari@qodefarm.com>
2025-07-14 11:22:54 +0530

    Testing part1

---
+++ apps/leads/test_leads.py
@@ -0,0 +1,15 @@
+def test_leads1():
+    mass = "hi"
+    assert mass == "hi", "Failed"
+
+def test_leads3():
+    mass = "hi"
+    assert mass == "hi", "Failed"
+
+def test_leads():
+    mass = "hi"
+    assert mass == "hi", "Failed"
+
+def test_leads2():
+    mass = "hi"
+    assert mass == "hi", "Failed"
---
+++ apps/users/user_test.py
@@ -0,0 +1,16 @@
+def test_user():
+    mass = "hi"
+    assert mass == "hi", "Failed"
+
+def test_user2():
+    mass = "Hello"
+    assert mass == "hi", "Failed"
+
+def test_user3():
+    mass = "hi"
+    assert mass == "hi", "Failed"
+
+def test_user4():
+    mass = "Hello"
+    print(mass)
+    assert mass == "hi", "Failed"
